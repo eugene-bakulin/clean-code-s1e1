e4862c8661c08ae30426c243aab31d32d39a4b99
eugene-bakulin <bakulin.eugene@gmail.com>
2022-05-28 22:21:44 +0400

    refactor: add alt attributes in index.html and app.js according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="buttons delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="del-img";
+  deleteButtonImg.alt="remove";
   deleteButton.appendChild(deleteButtonImg);
 
 
